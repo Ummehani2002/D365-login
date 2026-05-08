ab649d452ee7fe5f89ae2e47daa12bb8fcc0dc51
Umme Hani <haniumme698@gmail.com>
2026-05-08 15:13:21 +0400

    menu match remove

--- config/rbac_menu_match.php
+++ config/rbac_menu_match.php
@@ -2,28 +2,11 @@
 
 return [
     'modules' => [
-        ['key' => 'master.access', 'label' => 'Masters - Access', 'route_name' => 'masters.company.index'],
-        ['key' => 'modules.accounts-payable', 'label' => 'Accounts payable', 'route_name' => null],
-        ['key' => 'modules.accounts-receivable', 'label' => 'Accounts receivable', 'route_name' => null],
-        ['key' => 'modules.asset-management', 'label' => 'Asset management', 'route_name' => null],
-        ['key' => 'modules.audit-workbench', 'label' => 'Audit workbench', 'route_name' => null],
-        ['key' => 'modules.budgeting', 'label' => 'Budgeting', 'route_name' => null],
-        ['key' => 'modules.cash-and-bank-management', 'label' => 'Cash and bank management', 'route_name' => null],
-        ['key' => 'modules.common', 'label' => 'Common', 'route_name' => null],
-        ['key' => 'modules.consolidations', 'label' => 'Consolidations', 'route_name' => null],
-        ['key' => 'modules.cost-accounting', 'label' => 'Cost accounting', 'route_name' => null],
-        ['key' => 'modules.cost-management', 'label' => 'Cost management', 'route_name' => null],
-        ['key' => 'modules.credit-and-collections', 'label' => 'Credit and collections', 'route_name' => null],
-        ['key' => 'modules.electronic-invoice-integration-zatca', 'label' => 'Electronic Invoice integration (ZATCA)', 'route_name' => null],
-        ['key' => 'modules.expense-management', 'label' => 'Expense management', 'route_name' => null],
-        ['key' => 'modules.fixed-assets', 'label' => 'Fixed assets', 'route_name' => null],
-        ['key' => 'modules.general-ledger', 'label' => 'General ledger', 'route_name' => null],
-        ['key' => 'modules.hr-integration', 'label' => 'HR Integration', 'route_name' => null],
+        ['key' => 'super.access', 'label' => 'Super Admin - Full App Access', 'route_name' => null],
+        ['key' => 'settings.access', 'label' => 'Admin - Settings Access (Except Token/Credentials)', 'route_name' => 'settings.index'],
         ['key' => 'modules.project-management.item-issue', 'label' => 'Project Management - Item Issue', 'route_name' => 'modules.project-management.item-issue'],
         ['key' => 'modules.procurement.purch-req', 'label' => 'Procurement & Sourcing - Purchase Requisition', 'route_name' => 'modules.procurement.purch-req'],
         ['key' => 'modules.procurement.grn', 'label' => 'Procurement & Sourcing - Goods Receive Note', 'route_name' => 'modules.procurement.grn'],
-        ['key' => 'settings.access', 'label' => 'Admin - Settings Access (Except Token/Credentials)', 'route_name' => 'settings.index'],
-        ['key' => 'super.access', 'label' => 'Super Admin - Full App Access', 'route_name' => null],
     ],
 ];
 
